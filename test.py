bc3fdd9fad80c0be627bbc7d106cb251e067b420
reddyrohith49471 <reddyrohith20061902@gmail.com>
2025-12-14 10:58:15 +0530

    Kept VGG16 as base model and changed fc layer

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
 from cnnClassifier.logging.logging import logger
 from cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
