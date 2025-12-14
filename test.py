166ad44fb7eb94cce6d072f2838d1673ecb09564
reddyrohith49471 <reddyrohith20061902@gmail.com>
2025-12-14 11:32:20 +0530

    Model training Part Completed

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from cnnClassifier.logging.logging import logger
 from cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.model_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -20,6 +21,17 @@ try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
